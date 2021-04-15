95f47163e1843dc882f6d291b32aa5fe1e8873c3
DESKTOP-B96HMB5\mpasa <mpasanen1@hotmail.com>
2021-04-14 21:15:50 -0500

    Added navigaton to LoadOnDemand screen

---
+++ TeleLifeAdmin.and/LoadOnDemandActivity.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using System;
+
+namespace TeleLifeAdmin.and
+{
+    [Activity(Label = "Load On Demand")]
+    public class LoadOnDemandActivity : Activity
+    {
+        Button _nonScheduledPacingButton;
+        TextView _nonScheduledPacingTextView;
+
+        Button _loadAutomatedContactsButton;
+        TextView _loadAutomatedContactsTextView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.LoadOnDemand);
+            
+            FindViews();
+            LinkEventHandlers();
+        }
+
+        private void LinkEventHandlers()
+        {
+            _nonScheduledPacingButton.Click += NonScheduledPacing_Click;
+            _loadAutomatedContactsButton.Click += LoadAutomatedContacts_Click;
+        }
+
+        private void FindViews()
+        {
+            _nonScheduledPacingButton = FindViewById<Button>(Resource.Id.nonScheduledPacingButton);
+            _nonScheduledPacingTextView = FindViewById<EditText>(Resource.Id.nonScheduledPacingEditText);
+            _loadAutomatedContactsButton = FindViewById<Button>(Resource.Id.loadAutomatedContactsButton);
+            _loadAutomatedContactsTextView = FindViewById<EditText>(Resource.Id.loadAutomatedContactsEditText);
+        }
+
+        private void NonScheduledPacing_Click(object sender, EventArgs args)
+        {
+            var pacingAmount = int.Parse(_nonScheduledPacingTextView.Text);
+            Console.WriteLine($"pacing amount {pacingAmount}");
+        }
+
+        private void LoadAutomatedContacts_Click(object sender, EventArgs args)
+        {
+            var pacingAmount = int.Parse(_loadAutomatedContactsTextView.Text);
+            Console.WriteLine($"Load amount {pacingAmount}");
+        }
+    }
+}
--- TeleLifeAdmin.and/MainActivity.cs
+++ TeleLifeAdmin.and/MainActivity.cs
@@ -2,24 +2,56 @@
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using System;
 
 namespace TeleLifeAdmin.and
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    [Activity(Label = "TeleLife Admin", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private Button _dashboardButton;
+        private Button _loadOnDemandButton;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            FindViews();
+            LinkEventHandlers();
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void LinkEventHandlers()
+        {
+            _dashboardButton.Click += DashboardButton_Click;
+            _loadOnDemandButton.Click += LoadOnDemandButton_Click;
+        }
+
+        private void FindViews()
+        {
+            _dashboardButton = FindViewById<Button>(Resource.Id.dashboardButton);
+            _loadOnDemandButton = FindViewById<Button>(Resource.Id.loadOnDemandButton);
+        }
+        
+        private void DashboardButton_Click(object sender, EventArgs args)
+        {
+            var dashboardIntent = new Intent(this, typeof(DashboardActivity));
+            StartActivity(dashboardIntent);
+        }
+
+        private void LoadOnDemandButton_Click(object sender, EventArgs args)
+        {
+            var loadOnDemandIntent = new Intent(this, typeof(LoadOnDemandActivity));
+            StartActivity(loadOnDemandIntent);
         }
     }
 }
