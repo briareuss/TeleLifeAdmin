e130666710b3c696f47e6553d1bbdadfe84f293c
mpasa <mpasanen1@hotmail.com>
2021-11-12 09:50:15 -0600

    Added Progress Bar control to Dashboard layout

--- TeleLifeAdmin.and/DashboardActivity.cs
+++ TeleLifeAdmin.and/DashboardActivity.cs
@@ -2,12 +2,13 @@
 using Android.OS;
 using Android.Support.V4.Widget;
 using Android.Support.V7.Widget;
-using System;
+
 using TeleLifeAdmin.and.Adapters;
+
 
 namespace TeleLifeAdmin.and
 {
-    [Activity(Label = "Dashboard" )]
+    [Activity(Label = "Dashboard")]
     public class DashboardActivity : Activity
     {
         private RecyclerView _dashboardRecylerView;
@@ -15,16 +16,14 @@ namespace TeleLifeAdmin.and
         private DashboardAdapter _dashboardAdapter;
 
         private SwipeRefreshLayout _refresh;
-        
+
         protected async override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+            
             SetContentView(Resource.Layout.Dashboard);
 
-            _refresh = FindViewById<SwipeRefreshLayout>(Resource.Id.dashboardRefreshLayout);            
-            _dashboardRecylerView = FindViewById<RecyclerView>(Resource.Id.dashboardRecyclerView);
-
+            FindViews();          
             _dashboardLayoutManager = new LinearLayoutManager(this);
             _dashboardRecylerView.SetLayoutManager(_dashboardLayoutManager);
             _dashboardAdapter = new DashboardAdapter();
@@ -32,16 +31,20 @@ namespace TeleLifeAdmin.and
             await _dashboardAdapter.RetrieveDashboardValues();
 
             _dashboardRecylerView.SetAdapter(_dashboardAdapter);
-           
+
             _refresh.Refresh += async delegate (object sender, System.EventArgs e)
             {
                 _dashboardAdapter = new DashboardAdapter();
-                await _dashboardAdapter.RetrieveDashboardValues();                
+                await _dashboardAdapter.RetrieveDashboardValues();
                 _dashboardRecylerView.SetAdapter(_dashboardAdapter);
 
                 _refresh.Refreshing = false;
             };
-
+        }
+        private void FindViews()
+        {
+            _refresh = FindViewById<SwipeRefreshLayout>(Resource.Id.dashboardRefreshLayout);
+            _dashboardRecylerView = FindViewById<RecyclerView>(Resource.Id.dashboardRecyclerView);
         }
     }
 }
